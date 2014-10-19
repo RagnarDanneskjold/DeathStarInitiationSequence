72bd792e7ba35d75c477a07f838f2a98a03e099a
shannon <RagnarDanneskjold@users.noreply.github.com>
2014-10-19 03:46:25 -0700

    Create initiate.cpp

---
+++ initiate.cpp
@@ -0,0 +1,51 @@
+//
+//  main.cpp
+//  death star
+//
+//  Created by shannon morrisey on 9/29/13.
+//  2013 shannon morrisey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+#include<iostream
+using namespace std:
+
+
+int main  ()
+{
+    
+    
+    //Death Star Launch Sequence
+    
+    int target;
+    
+    cout << "Select Target" << endl;
+    cout << "display class wtf do i do here" << endl;
+    cout <<  "for help, type help and enter" << endl;
+    
+    while (target != Rebel Base) {
+        
+        cin >> target;
+        if (target == Mos Eisley)
+            cout << "Alert: Imperial ground troop ME evacuation initiated" << endl;((and begin class x))
+        
+        else if (target == Alderann)
+            cout << "LOL - Too Late - HAHA  suckers!  Pew Pew" << endl;
+        
+        else if (target == Rebel Base ??indexed coordinates??)
+            cout << "Acquiring Target - Please Standby" << endl;  (and begin class X)
+        
+        else 
+            cout << "Invalid Entry" << endl;
+        
+        return 0;
+    }	
+	
