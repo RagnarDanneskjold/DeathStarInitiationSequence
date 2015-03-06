978f3162ce5b634a268bcdf81d4996899451815c
shannon <RagnarDanneskjold@users.noreply.github.com>
2015-03-05 22:27:48 -0800

    Update initiate.cpp

--- initiate.cpp
+++ initiate.cpp
@@ -1,20 +1,25 @@
+
+//
+//         DO AS YOU PLEASE IMMUNITY
+DAYP-Immunity Do As You Please Immunity
+
+*Not a license; jurisdiction-agnostic 1425622245//
+
+     DO AS YOU PLEASE IMMUNITY 
+                Version N-1
+Everyone is permitted to copy and distribute verbatim or modified copies of this document.
+
+         DO AS YOU PLEASE IMMUNITY N-1 
+TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+1. DO AS YOU PLEASE.
+2. DO AS YOU PLEASE.
+
+
+
 //
 //  main.cpp
 //  death star
-//
-//         DO AS YOU PLEASE PUBLIC LICENSE 
-                    Version X 
-
-
-
- Everyone is permitted to copy and distribute verbatim or modified 
- copies of this document. 
-
-             DO AS YOU PLEASE LICENSE VX 
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
-  0.  DO AS YOU PLEASE.
-  1.  DO AS YOU PLEASE.
-
 //
 
 #include <iostream>
